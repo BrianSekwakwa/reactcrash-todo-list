8c91803f557e3fcfce18313774cc0ee17e55319f
Brian <briansenpai14@gmail.com>
2019-03-07 17:16:39 +0200

    Updates

---
+++ precache-manifest.6c26622a4781fa842056ea563074bb9c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7d8e5192842832af2e9a",
+    "url": "/reactcrash-todo-list/static/js/runtime~main.7d8e5192.js"
+  },
+  {
+    "revision": "32eedd4a9765a93d4569",
+    "url": "/reactcrash-todo-list/static/js/main.32eedd4a.chunk.js"
+  },
+  {
+    "revision": "6f96e78ba8e11d79daff",
+    "url": "/reactcrash-todo-list/static/js/1.6f96e78b.chunk.js"
+  },
+  {
+    "revision": "32eedd4a9765a93d4569",
+    "url": "/reactcrash-todo-list/static/css/main.9862869e.chunk.css"
+  },
+  {
+    "revision": "a93025c37376976c2fa293ba36d21a44",
+    "url": "/reactcrash-todo-list/index.html"
+  }
+];
